c57c19f8c0adc9df9599121142e00fe119f362f4
Manish Kumar <155321591+manishkumar620@users.noreply.github.com>
2026-06-30 17:15:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findUnique(int[] arr) {
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++) {
+            sum = sum ^ arr[i];
+        }
+        return sum;
+    }
+}
